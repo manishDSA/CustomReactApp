561b1deb28ae4833edb788485b7f29258e28a28e
manishDSA <manishsolanki7879@gmail.com>
2024-10-04 02:30:38 +0530

    optimisecode

--- CustomReact/customReact.js
+++ CustomReact/customReact.js
@@ -1,11 +1,20 @@
 
 function customRender(rectEelement,container){
+    /*
     const DomEelenment= document.createElement(rectEelement.type);
     DomEelenment.innerHTML=rectEelement.children;
     DomEelenment.setAttribute('href', rectEelement.props.href)
     DomEelenment.setAttribute('target',rectEelement.props.target)
 container.appendChild(DomEelenment)
+*/
 
+const DomEelement = document.createElement(rectEelement.type);
+DomEelement.innerHTML = rectEelement.children;
+for (const prop in rectEelement["props"]) {
+    if(prop=='children') continue
+    DomEelement.setAttribute(prop,rectEelement.props[prop])
+}
+container.appendChild(DomEelement)
 }
 
 
